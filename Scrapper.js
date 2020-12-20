447a863db038f751363ee39710b4be84329c7cdf
Lionel K <quantumlionel@gmail.com>
2020-12-20 19:40:26 +0100

    adding scrapper

---
+++ Scrapper.js
@@ -0,0 +1,35 @@
+function getthefucknwebsite2POST() {
+  let acturl = 'https://en.wikipedia.org/wiki/Web_scraping';
+  let url = `https://proxybot.io/api/v1/s3MxG9sFddfgxio6Ok6KmomgYdr1?render_js=true&url=${acturl}`;
+
+  var raw = "[\r\n  {\r\n    \"selector\": \"#firstHeading\",\r\n    \"get\": \"text\"\r\n  },\r\n  {\r\n    \"selector\": \"#siteSub\",\r\n    \"get\": \"text\"\r\n  }\r\n]";
+
+  var requestOptions = {
+    method: 'POST',
+    body: raw
+  };
+
+  fetch(url, requestOptions)
+  .then(response => response.json())
+  .then(res => console.log(res))
+  .catch(error => console.log(error))
+};
+
+function ScrapWebsite() {
+  var raw = "[\r\n    {\r\n        \"selector\": \"#firstHeading\",\r\n        \"get\": \"text\"\r\n    },\r\n    {\r\n        \"selector\": \"#siteSub\",\r\n        \"get\": \"text\"\r\n    }\r\n]";
+
+  var requestOptions = {
+    method: 'POST',
+    body: raw,
+    redirect: 'follow'
+  };
+
+  fetch("https://proxybot.io/api/v1/s3MxG9sFddfgxio6Ok6KmomgYdr1?url=https://en.wikipedia.org/wiki/Web_scraping", requestOptions)
+    .then(response => response.text())
+    .then(res => {document.getElementById('demo').innerHTML=res})
+    .catch(error => {document.getElementById('demo').innerHTML=error});
+}
+
+document.querySelector("button").addEventListener("click", function(){
+  ScrapWebsite();
+}, false);
