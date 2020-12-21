748443be3b2b01e6fdd6b97594e87ddf465ecc0f
Lionel K <quantumlionel@gmail.com>
2020-12-21 18:18:12 +0100

    original website html

--- Scrapper.js
+++ Scrapper.js
@@ -1,35 +1,55 @@
-function getthefucknwebsite2POST() {
-  let acturl = 'https://en.wikipedia.org/wiki/Web_scraping';
-  let url = `https://proxybot.io/api/v1/s3MxG9sFddfgxio6Ok6KmomgYdr1?render_js=true&url=${acturl}`;
-
-  var raw = "[\r\n  {\r\n    \"selector\": \"#firstHeading\",\r\n    \"get\": \"text\"\r\n  },\r\n  {\r\n    \"selector\": \"#siteSub\",\r\n    \"get\": \"text\"\r\n  }\r\n]";
-
+async function ScrapWebsite(url) {
   var requestOptions = {
-    method: 'POST',
-    body: raw
-  };
-
-  fetch(url, requestOptions)
-  .then(response => response.json())
-  .then(res => console.log(res))
-  .catch(error => console.log(error))
-};
-
-function ScrapWebsite() {
-  var raw = "[\r\n    {\r\n        \"selector\": \"#firstHeading\",\r\n        \"get\": \"text\"\r\n    },\r\n    {\r\n        \"selector\": \"#siteSub\",\r\n        \"get\": \"text\"\r\n    }\r\n]";
-
-  var requestOptions = {
-    method: 'POST',
-    body: raw,
+    method: 'GET',
     redirect: 'follow'
   };
-
-  fetch("https://proxybot.io/api/v1/s3MxG9sFddfgxio6Ok6KmomgYdr1?url=https://en.wikipedia.org/wiki/Web_scraping", requestOptions)
+  let res; 
+  var x = await fetch("https://cors-anywhere.herokuapp.com/"+url, requestOptions)
     .then(response => response.text())
-    .then(res => {document.getElementById('demo').innerHTML=res})
-    .catch(error => {document.getElementById('demo').innerHTML=error});
-}
+    .then(result => {
+      var st = result.search("Available from")+46;
+      var end = result.search("€</dd>")-1;
+      res = result.substring(st,end);
+    })
+    .catch(error => {res = 'error'});
+  return res;
+};
 
-document.querySelector("button").addEventListener("click", function(){
-  ScrapWebsite();
-}, false);
+let cardsLP = ['Arcane-Signet', 'Command-Tower'];
+let investorLP = ['Lolz', 'Pops'];
+let investmentLP = ['1,01','2,00'];
+let deliveryfeesLP = ['0,10','0,02'];
+
+window.onload = async function() {
+  document.getElementById('table').innerHTML = `<tr>
+  <th>Card Name</th>
+  <th>Investor</th>
+  <th>Investment (Euros)</th>
+  <th>Delivery Fees (Euros)</th>
+  <th>Current Lowest Price (Euros)</th>
+  <th>Profit (Euros)</th>
+  <th>Profit Minus Delivery Fees (Euros)</th>
+  </tr>`;
+  for (i=0; i<cardsLP.length; i++) {
+    var cardname = cardsLP[i].replace('-', ' ');
+    var cardurl = `https://www.cardmarket.com/en/Magic/Cards/${cardsLP[i]}?language=1&minCondition=2&isSigned=N&isAltered=N`;
+    document.getElementById('table').innerHTML += `<tr>
+    <td><a href = \'${cardurl}\'>${cardname}</a></td>
+    <td>${investorLP[i]}</td>
+    <td>${investmentLP[i]}</td>
+    <td>${deliveryfeesLP[i]}</td>
+    <td id = \'${cardsLP[i]}\'><em>Loading...</em></td>
+    <td id = \'${cardsLP[i]}Profit\'><em>Loading...</em></td>
+    <td id = \'${cardsLP[i]}ProfitMinus\'><em>Loading...</em></td>
+    </tr>`;
+  };
+  for (i=0; i<cardsLP.length; i++) {
+    var cardurl = `https://www.cardmarket.com/en/Magic/Cards/${cardsLP[i]}?language=1&minCondition=2&isSigned=N&isAltered=N`;
+    document.getElementById(cardsLP[i]).innerHTML = await ScrapWebsite(cardurl)
+  };
+  //document.getElementById('test').innerHTML = await ScrapWebsite(cardurl);
+};
+
+//document.querySelector("button").addEventListener("click", function(){
+//  ScrapWebsite();
+//}, false);
